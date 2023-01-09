8e204b427c87c4c750e7924c519dbf89e49d75c3
MystikHub <13604102+MystikHub@users.noreply.github.com>
2023-01-09 18:42:16 +0000

    Modified precision and accuracy metrics to ignore masks

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -678,8 +678,8 @@ def compute_matches(gt_boxes, gt_class_ids, gt_masks,
     pred_scores = pred_scores[indices]
     pred_masks = pred_masks[..., indices]
 
-    # Compute IoU overlaps [pred_masks, gt_masks]
-    overlaps = compute_overlaps_masks(pred_masks, gt_masks)
+    # Compute IoU overlaps [pred_boxes, gt_boxes]
+    overlaps = compute_overlaps(pred_boxes, gt_boxes)
 
     # Loop through predictions and find matching ground truth boxes
     match_count = 0
